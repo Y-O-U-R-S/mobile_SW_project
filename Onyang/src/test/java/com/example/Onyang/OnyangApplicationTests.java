069a46cdcba4a78422c049fc5e5104431b42d6b7
nahollo <skgudwlsdlrj@gmail.com>
2024-09-05 13:06:29 +0900

    first commit
    first commit

---
+++ Onyang/src/test/java/com/example/Onyang/OnyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Onyang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
