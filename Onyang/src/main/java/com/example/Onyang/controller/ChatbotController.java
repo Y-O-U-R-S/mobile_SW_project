5bec30a9d211cac1f7846ad20ec397815910bdac
nahollo <skgudwlsdlrj@gmail.com>
2024-12-03 15:09:31 +0900

    hello
    hello

--- Onyang/src/main/java/com/example/Onyang/controller/ChatbotController.java
+++ Onyang/src/main/java/com/example/Onyang/controller/ChatbotController.java
@@ -10,19 +10,18 @@ import org.springframework.http.ResponseEntity;
 import com.example.Onyang.model.Chatbot;
 import com.example.Onyang.service.ChatbotService;
 
-
 // CORS 설정 : 특정 출처에서의 요청 허용
 @CrossOrigin(origins = "http://localhost:3000")
 // RESTful 웹서비스의 컨트롤러, 메소드의 반환 값이 자동으로 JSON 형식으로 변환됨
 @RestController
 // 이 클래스의 모든 메소드가 /chatbot 경로로 시작하는 HTTP 요청을 처리하도록 설정
 @RequestMapping("/chatbot")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ChatbotController {
 
     // 의존성 주입 chatbotservice 인스턴스를 주입하여 서비스 로직 호출
     @Autowired
     private ChatbotService chatbotService;
+
     @GetMapping("/email/{email}")
     public ResponseEntity<List<Chatbot>> getMessagesByEmail(@PathVariable("email") String email) {
         List<Chatbot> chatbots = chatbotService.findByEmail(email);
